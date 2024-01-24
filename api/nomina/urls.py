7ab1e976a5e7137e2b21fbedf5a95bc82a5f2410
julifaga02 <mara.julif@gmail.com>
2024-01-24 20:17:33 -0300

    creacion de views

--- api/nomina/urls.py
+++ api/nomina/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import GetEmpleadoView
+from .views import GetEmpleadoView, CreateEmployeView, ModifierEmployeView, DeleteEmployeView
 
 urlpatterns = [
     path('listar-empleados/', GetEmpleadoView.as_view(), name='lista_empleados'),
+    path('crear-empleados/', CreateEmployeView.as_view(), name='crear_empleados'),
+    path('modificar-empleados/<int:id>', ModifierEmployeView.as_view(),name='modificar_empleados'),
+    path('borrar-empleados/<int:id>', DeleteEmployeView.as_view(), name='borrar_empleados'),
+
 ]
--- api/nomina/views.py
+++ api/nomina/views.py
@@ -3,12 +3,36 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import EmpleadoSerializer
 from .models import Empleado
+from rest_framework.generics import get_object_or_404
 
 class GetEmpleadoView(APIView):
     def get(self, request):
-        queryset= Empleado.objects.all()
-        serializer= EmpleadoSerializer(queryset, many=True)
+        queryset = Empleado.objects.all()
+        serializer = EmpleadoSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+class CreateEmployeView(APIView):
+    def post(self, request):
+        serializer = EmpleadoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+class ModifierEmployeView(APIView):
+    def put(self,request,id):
+        empleado= get_object_or_404(Empleado, pk=id)
+        serializer = EmpleadoSerializer(empleado,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+class DeleteEmployeView(APIView):
+    def delete(self,request,id):
+        empleado= get_object_or_404(Empleado, pk=id)
+        empleado.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT) 
