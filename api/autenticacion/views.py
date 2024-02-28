35383e29857cee15d5853c16a822889ef97a4922
Iran Mederos <iranmederos@gmail.com>
2024-02-28 20:10:24 -0300

    limpiando importaciones

--- api/autenticacion/views.py
+++ api/autenticacion/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Role
-from rest_framework.permissions import BasePermission
 from .serializers import SerializerUser
 from rest_framework import status
 
