6b3dfcea7283ec04718e657b0287036eb30297c6
Iran Mederos <iranmederos@gmail.com>
2024-03-14 19:29:27 -0300

    installed whitenoise

--- api/settings.py
+++ api/settings.py
@@ -57,7 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.staticfiles.middleware.StaticFilesMiddleware'
+    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'api.urls'
